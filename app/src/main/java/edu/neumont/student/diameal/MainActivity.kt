c17c7139a40a27027bf740c518e5d3610d7ee943
Donald Williams <60931728+don-williams@users.noreply.github.com>
2020-09-25 04:10:28 -0600

    Initial project setup
    Signed-off-by: Donald Williams <60931728+Don-Williams@users.noreply.github.com>

---
+++ app/src/main/java/edu/neumont/student/diameal/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.neumont.student.diameal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
